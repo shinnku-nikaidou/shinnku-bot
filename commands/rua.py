902caf808d491e1e21aac088188e6703ad0801b8
shinnku <nikaidou@shinnku.com>
2023-08-23 00:34:52 -0400

    add sticker const

--- commands/rua.py
+++ commands/rua.py
@@ -6,7 +6,7 @@ from telegram.constants import ChatAction
 from telegram.ext import ContextTypes
 
 from utils.decorators import send_action
-
+from constants import sticker
 import random
 
 # Init logger
@@ -16,10 +16,7 @@ logger = getLogger(__name__)
 
 @send_action(ChatAction.TYPING)
 async def rua(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
-    # pylint: disable=unused-argument
-    sticker_id_1 = "CAACAgUAAx0CU2B2vQACBolk5FUDwuIQA1PPB7U6CP3zYb5gbQAC7ggAAvXI0FeiEnu0PWSsdjAE"  # 😧
-    sticker_id_2 = "CAACAgUAAx0CU2B2vQACBo9k5FZ6DB6ZsFTsi1JgoDeob4kWFQACcwoAAlpg0FeY_SJn5kLnjjAE"  # 😫
     if random.random() < 0.5:
-        await update.message.reply_sticker(sticker=sticker_id_1)
+        await update.message.reply_sticker(sticker=sticker.shinnku[0])
     else:
-        await update.message.reply_sticker(sticker=sticker_id_2)
+        await update.message.reply_sticker(sticker=sticker.shinnku[1])
---
+++ constants/sticker.py
@@ -0,0 +1,4 @@
+shinnku = [
+    "CAACAgUAAx0CU2B2vQACBolk5FUDwuIQA1PPB7U6CP3zYb5gbQAC7ggAAvXI0FeiEnu0PWSsdjAE",  # 😧
+    "CAACAgUAAx0CU2B2vQACBo9k5FZ6DB6ZsFTsi1JgoDeob4kWFQACcwoAAlpg0FeY_SJn5kLnjjAE",  # 😫
+]
